ab3c8b252d7515d6ce615b191316682bc0ea82c9
ANDREW L <andrewbca2003@gmail.com>
2026-01-21 10:13:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/andrew2003-error.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
